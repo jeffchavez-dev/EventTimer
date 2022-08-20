4f3a9c37801ea59cd26afee75b9179d81b8dfb0f
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-20 11:50:03 +0800

    Start JS script

---
+++ script.js
@@ -0,0 +1,12 @@
+const myEvent = document.querySelector(".myEvent")
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+
+let newYears = "";
+
+const myTimer = () => {
+    
+}
